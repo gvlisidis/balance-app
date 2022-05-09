d8f0dd4acb72eed09224f3a2dde25963b10fa68a
George  Vlisidis <gblisidis@gmail.com>
2022-05-10 00:37:15 +0100

    Installed Laravel Excel package

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Expense;
 use Illuminate\Http\Request;
 
 class ExpenseController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('expenses.index', [
+            'expenses' => Expense::all(),
+        ]);
+    }
+
+    public function create()
+    {
+        return view('expenses.create');
+    }
+
+    public function store(Request $request)
+    {
+        dd($request->file('monthly_expenses'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ExpenseController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,6 +12,15 @@ Route::middleware(['auth'])->group(function (){
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::prefix('expenses')->name('expenses.')->group(function (){
+        Route::get('', [ExpenseController::class, 'index'])->name('index');
+        Route::get('create', [ExpenseController::class, 'create'])->name('create');
+        Route::post('', [ExpenseController::class, 'store'])->name('store');
+        Route::get('/{expense}', [ExpenseController::class, 'edit'])->name('edit');
+        Route::patch('/{expense}', [ExpenseController::class, 'update'])->name('update');
+        Route::delete('/{expense}', [ExpenseController::class, 'delete'])->name('delete');
+    });
 
     Route::prefix('categories')->name('categories.')->group(function (){
         Route::get('', [CategoryController::class, 'index'])->name('index');
